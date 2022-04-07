3e17ace889d7a40ddc34ff2e639e9f135d09f979
Silvio Cornejo <silvio.cornejo.est@tecazuay.edu.ec>
2022-04-06 20:34:56 -0500

    esta es una prueba de primer programa

---
+++ src/main/java/ejemplo/com/sistemaPrueb/SistemaPruebApplication.java
@@ -0,0 +1,13 @@
+package ejemplo.com.sistemaPrueb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPruebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPruebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ejemplo/com/sistemaPrueb/SistemaPruebApplicationTests.java
@@ -0,0 +1,13 @@
+package ejemplo.com.sistemaPrueb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPruebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
